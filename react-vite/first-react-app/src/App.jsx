c959c8cb7d758a5b0b74549a39f39df5e44d3154
ASANA MALTITI ABDULAI <asanamaltitia@gmail.com>
2026-05-28 22:45:13 -0400

    Create first React Vite app

---
+++ react-vite/first-react-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() { 
+return ( 
+<main> 
+<h1>My First React App</h1> 
+<p>This is my first React + Vite project.</p> 
+<p>I am learning how to build modern frontend applications.</p> 
+</main> 
+); 
+} 
+export default App;
